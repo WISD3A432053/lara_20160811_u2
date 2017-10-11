2220707c748802806f10326545e68a4bfb824ac5
M511-30\admin <s3a432053@student.ncut.edu.tw>
2017-10-11 12:23:18 +0800

    修改 resources/views/welcome.blade.php的內容，增加一個連結

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
                     @endif
                 </div>
             @endif
-
+                <a href ="{{route('hello.index')}}">Hello</a>
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
